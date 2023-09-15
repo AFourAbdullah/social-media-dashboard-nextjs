8f6cf33e7da8f3ede1aafdce3c5081fb7eec8891
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-15 19:32:45 +0500

    files added

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,7 +1,56 @@
+"use client";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerHeader,
+  DrawerCloseButton,
+  DrawerFooter,
+  DrawerContent,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
-};
+export default function Sidebar() {
+  const { isOpen = true, onOpen, onClose } = useDisclosure();
+  const btnRef = React.createRef<HTMLButtonElement>();
 
-export default Sidebar;
+  return (
+    <>
+      <Button
+        ref={btnRef}
+        colorScheme="teal"
+        className="bg-black"
+        onClick={onOpen}
+        alignSelf="left"
+        left="10"
+        position="absolute"
+        top="3"
+      >
+        Open
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody></DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
